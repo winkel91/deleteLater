7459e8f5a0c95f305e637d31e39235bbc854f973
Tobias Winkel <winkeltobias@gmail.com>
2023-05-10 09:52:35 +0200

    2nd

--- src/main/java/winkel/deletelater/GreetingController.java
+++ src/main/java/winkel/deletelater/GreetingController.java
@@ -14,7 +14,7 @@ public class GreetingController {
     }
     @GetMapping("/")
     public ResponseEntity<Greeting> greeting(){
-        Greeting greeting = new Greeting("goddag fra server");
+        Greeting greeting = new Greeting("godnat fra server");
         greetingRepository.save(greeting);
         return new ResponseEntity<>(greeting, HttpStatus.OK);
     }
