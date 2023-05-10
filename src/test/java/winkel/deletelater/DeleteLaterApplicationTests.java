9524ef3a615d07831f4e915fab645431c51bc427
Tobias Winkel <winkeltobias@gmail.com>
2023-05-10 09:59:09 +0200

    3rd

--- src/test/java/winkel/deletelater/DeleteLaterApplicationTests.java
+++ src/test/java/winkel/deletelater/DeleteLaterApplicationTests.java
@@ -3,10 +3,10 @@ package winkel.deletelater;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DeleteLaterApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
